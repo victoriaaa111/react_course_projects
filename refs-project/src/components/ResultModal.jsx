9b2e8b46de1bf013afdd2cdfbf658bf979d86cbc
victoriaaa111 <victoria.mutruc@gmail.com>
2025-07-29 18:39:14 +0300

    Add score in modal in case player did not lose

--- refs-project/src/components/ResultModal.jsx
+++ refs-project/src/components/ResultModal.jsx
@@ -4,9 +4,11 @@ const ResultModal =  forwardRef(
     function ResultModal({result, targetTime, remainingTime, onReset}, ref) {
         const userLost = remainingTime <= 0;
         const formRemainingTime = (remainingTime / 1000).toFixed(2);
+        const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
         return (
             <dialog ref={ref} className="result-modal">
                 {userLost && <h2>You lost</h2>}
+                {!userLost && <h2>Your score: {score} %</h2>}
                 <p>The target time was <strong>{targetTime} seconds left.</strong></p>
                 <p>You stopped the timer at <strong>{formRemainingTime} seconds left.</strong></p>
                 <form method="dialog" onSubmit={onReset}>
