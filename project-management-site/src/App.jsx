1636e281b4217d67517bf0aa061605afdf367ed4
victoriaaa111 <victoria.mutruc@gmail.com>
2025-07-31 14:27:07 +0300

    Add landing page

--- project-management-site/src/App.jsx
+++ project-management-site/src/App.jsx
@@ -1,13 +1,15 @@
-
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-      <h1>Project Management App</h1>
-    </>
+    <div className="flex flex-col items-center justify-center -translate-y-16 min-h-screen gap-4">
+        <h1 className= "inline-block text-[#3E2723] text-7xl font-medium  font-roxb border-2 border-[#3E2723] rounded-full px-12 py-12">Project Management App</h1>
+        <p className="text-[#3E2723] text-2xl font-montserrat underline underline-offset-8">Plan, track, and manage your projects effortlessly.</p>
+    </div>
+
+
   )
 }
 
