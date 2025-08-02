95c6d357870ab34c5f689d8c2f70d87f0164281f
victoriaaa111 <victoria.mutruc@gmail.com>
2025-08-02 21:41:40 +0300

    Change from useState to useRef in Tasks to add modal when input invalid

--- project-management-site/src/components/Tasks.jsx
+++ project-management-site/src/components/Tasks.jsx
@@ -1,15 +1,26 @@
 
-import {useState} from "react";
+import {useRef} from "react";
+import Modal from "./Modal.jsx";
 export default function Tasks({tasks, onAddTask, onDeleteTask}) {
-    const[enteredTask, setEnteredTask] = useState('');
-    function handleChange(e){
-        setEnteredTask(e.target.value);
-    }
+    const enteredTaskRef = useRef();
+    const modal = useRef('');
+
     function handleClick(){
+        const enteredTask = enteredTaskRef.current.value;
+        if (enteredTask.trim()===''){
+            modal.current.open()
+            return;
+        }
         onAddTask(enteredTask);
-        setEnteredTask('');
+        enteredTaskRef.current.value = '';
     }
     return(
+        <>
+        <Modal ref={modal} buttonCaption="Close">
+            <h2 className="mb-4 font-roxb font-bold uppercase md:text-2xl text-[#3E2723]">Invalid Input</h2>
+            <p className="text-l font-montserrat text-[#3E2723] font-bold">You forgot to enter a value for the task text.</p>
+            <p className="text-l font-montserrat text-[#3E2723] font-bold">Please make sure you provide a valid input.</p>
+        </Modal>
         <section>
             <h2>Tasks</h2>
             {tasks.length === 0 &&
@@ -26,9 +37,10 @@ export default function Tasks({tasks, onAddTask, onDeleteTask}) {
                 ))}
             </ul>
             <div className="flex items-center justify-between ">
-                <input onChange={handleChange} value={enteredTask} className="bg-stone-200" type="text"/>
+                <input ref={enteredTaskRef} className="bg-stone-200" type="text"/>
                 <button onClick={handleClick}>Add Task</button>
             </div>
         </section>
+            </>
     )
 }
