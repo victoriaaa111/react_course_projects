43e89d50b5433f92016c19d92fc9940cce850d40
victoriaaa111 <victoria.mutruc@gmail.com>
2025-08-02 20:37:03 +0300

    Add SelectedProject Component to show the details of a clicked project on the right

--- project-management-site/src/components/DashBoard.jsx
+++ project-management-site/src/components/DashBoard.jsx
@@ -2,6 +2,7 @@ import Sidebar from "./Sidebar.jsx";
 import NewProject from "./NewProject.jsx";
 import NoProjectSelected from "./NoProjectSelected.jsx";
 import {useState} from "react";
+import SelectedProject from "./SelectedProject.jsx";
 
 export default function DashBoard() {
     const [projectsState, setProjectsState] = useState({
@@ -32,7 +33,17 @@ export default function DashBoard() {
         })
     }
 
-    let content;
+    function handleSelectProject(id) {
+        setProjectsState(prevState=>{
+            return {
+                ...prevState,
+                selectedProjectId: id
+            }
+        })
+    }
+
+    const selectedProject = projectsState.projects.find(project=> project.id === projectsState.selectedProjectId);
+    let content = <SelectedProject project={selectedProject}/>;
     if(projectsState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject}/>
     }else if(projectsState.selectedProjectId === undefined) {
@@ -41,7 +52,7 @@ export default function DashBoard() {
 
 return(
     <div className="flex gap-8 bg-[#F4C9D6]">
-    <Sidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+    <Sidebar onStartAddProject={handleStartAddProject} onSelectProject={handleSelectProject} selectedProjectId={projectsState.selectedProjectId} projects={projectsState.projects}/>
         {content}
     </div>
 )
